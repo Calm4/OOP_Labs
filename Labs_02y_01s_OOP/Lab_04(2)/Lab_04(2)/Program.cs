6cee37afe1e11126161ed344bec91b42e34f9345
Danila Buyakevich <88559799+Calm4@users.noreply.github.com>
2022-08-01 01:03:58 +0300

    Add files via upload

---
+++ Labs_02y_01s_OOP/Lab_04(2)/Lab_04(2)/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Praktika_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            string primer = a + " ~ " + b;
+
+            StringBuilder[] sb = new StringBuilder[4];
+
+            for (int i = 0; i < sb.Length; i++)
+            {
+                sb[i] = new StringBuilder(primer);
+            }
+            string[] ugu = { "+", "-", "*", "/" };
+            for (int i = 0; i < sb.Length; i++)
+            {
+                sb[i].Replace("~", ugu[i]);
+            }
+            Console.WriteLine(primer);
+            Console.WriteLine("----------------");
+
+            foreach (var item in sb)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
