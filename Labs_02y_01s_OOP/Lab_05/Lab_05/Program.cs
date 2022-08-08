5afa498d71d2441ab2c5ee8f04b2917e04b5005e
Danila Buyakevich <88559799+Calm4@users.noreply.github.com>
2022-08-08 22:59:36 +0300

    Add files via upload

---
+++ Labs_02y_01s_OOP/Lab_05/Lab_05/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace Lab_05
+{
+    internal class Program
+    {
+        // раскидать по методам
+        static void Main(string[] args)
+        {
+            string[] newFile = File.ReadAllLines($"Examples/1.txt");
+            for (int i = 0; i < newFile.Length; i++)
+            {
+                Console.WriteLine(newFile[i]);
+            }
+            Console.WriteLine("==================================");
+            Match[] match = new Match[newFile.Length];
+            for (int i = 0; i < newFile.Length; i++)
+            {
+                match[i] = Regex.Match(newFile[i], "\\((.*?)\\+(.*?)\\)\\*(.*?)\\-\\((.*)\\)");
+            }
+            int number1, number2, number3, number4, result = 0;
+
+            for (int i = 0; i < newFile.Length; i++)
+            {
+
+                number1 = Convert.ToInt32(match[i].Groups[1].ToString());
+                number2 = Convert.ToInt32(match[i].Groups[2].ToString());
+                number3 = Convert.ToInt32(match[i].Groups[3].ToString());
+                number4 = Convert.ToInt32(match[i].Groups[4].ToString());
+                result = (number1 + number2) * number3 - number4;
+                Console.WriteLine(result);
+            }
+
+        }
+    }
+}
