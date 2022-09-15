8fdeed6488bd3b470ffba0e890b98eaf93b00088
Calmbl4 <boss.buyakevich@mail.ru>
2022-09-15 18:26:44 +0300

    start fix 2nd lab

---
+++ Labs_02y_01s_OOP/Lab_02(2)/Lab_02(2)/Vector.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_02_2_
+{
+    internal class Vector
+    {
+        public int VectorX { get; set; }
+        public int VectorY { get; set; }
+        public int VectorZ { get; set; }
+
+
+        public static void ShowResult()
+        {
+        }
+
+        public static Vector operator +(Vector vector1, Vector vector2)
+        {
+
+            return new Vector
+            {
+                VectorX = vector1.VectorX + vector2.VectorX,
+                VectorY = vector1.VectorY + vector2.VectorY,
+                VectorZ = vector1.VectorZ + vector2.VectorZ
+            };
+        }
+        public static Vector operator -(Vector vector1, Vector vector2)
+        {
+
+            return new Vector
+            {
+                VectorX = vector1.VectorX - vector2.VectorX,
+                VectorY = vector1.VectorY - vector2.VectorY,
+                VectorZ = vector1.VectorZ - vector2.VectorZ
+            };
+        }
+        public static Vector operator *(Vector vector1, Vector vector2)
+        {
+            return new Vector
+            {
+                VectorX = vector1.VectorY * vector2.VectorZ - vector1.VectorZ * vector2.VectorY,
+                VectorY = -(vector1.VectorX * vector2.VectorZ - vector1.VectorZ * vector2.VectorX),
+                VectorZ = vector1.VectorX * vector2.VectorY - vector1.VectorY * vector2.VectorX
+            };
+        }
+        /*public static  operator *(Vector vector1,Vector vector2,Vector vector3)
+        {
+            return vector1.VectorX * vector2.VectorX + vector1.VectorY * vector2.VectorY + vector1.VectorZ * vector2.VectorZ;
+        }*/
+    }
+}
