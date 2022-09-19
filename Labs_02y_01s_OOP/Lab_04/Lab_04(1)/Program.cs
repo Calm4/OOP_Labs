9aa48dc647c9964f317f4165623ab9edbebbfc3d
Calmbl4(laptop) <boss.buyakevich@mail.ru>
2022-09-19 16:14:58 +0300

    reworked 4th lab

---
+++ Labs_02y_01s_OOP/Lab_04/Lab_04(1)/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktika_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string String = "кот шалаш луна поле кок кровосток небо апельсин сосок";
+
+            string[] Words = String.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Список ваших слов");
+            foreach (var item in Words)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(item);
+            }
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Условие: Первая и последняя буква слова совподают");
+            Console.WriteLine("Если слово подсвечено ЗЕЛЕНЫМ значит оно подходит под ваше условие");
+            Console.WriteLine("Если слово подсвечено КРАСНЫМ значит оно НЕ подходит под ваше условие");
+            for (int i = 0; i < Words.Length; i++)
+            {
+
+                char[] Letters = Words[i].ToCharArray();
+                for (int j = 0; j < Letters.Length; j++)
+                {
+                    if (Letters[0] == Letters[Letters.Length - 1])
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(Letters[j]);
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.Write(Letters[j]);
+                    }
+                }
+                Console.WriteLine();
+
+
+
+
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+
+
+        }
+    }
+}
---
+++ Labs_02y_01s_OOP/Lab_04/Lab_04(2)/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_04_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            string primer = a + " ~ " + b;
+
+            StringBuilder[] sb = new StringBuilder[4];
+
+            for (int i = 0; i < sb.Length; i++)
+            {
+                sb[i] = new StringBuilder(primer);
+            }
+            string[] ugu = { "+", "-", "*", "/" };
+            for (int i = 0; i < sb.Length; i++)
+            {
+                sb[i].Replace("~", ugu[i]);
+            }
+            Console.WriteLine(primer);
+            Console.WriteLine("----------------");
+
+            foreach (var item in sb)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
