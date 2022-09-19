dc73de223c584747b11327ff5cb543642d39364c
Calmbl4 <boss.buyakevich@mail.ru>
2022-09-19 21:16:16 +0300

    some changes

--- Labs_02y_01s_OOP/Lab_02(Operators)/Lab_02(2)/Program.cs
+++ Labs_02y_01s_OOP/Lab_02(Operators)/Lab_02(2)/Program.cs
@@ -12,27 +12,35 @@ namespace Lab_02_2_
         {
             Vector vector1 = new Vector() { VectorX = 1, VectorY = 3, VectorZ = 5 };
             Vector vector2 = new Vector() { VectorX = 2, VectorY = 4, VectorZ = 6 };
-           
+
             Vector vectorAddition = vector1 + vector2;
             Vector vectorSubtraction = vector1 - vector2;
             Vector vectorVectorMultiplication = vector1 * vector2;
-            
+
             int choice = 0;
-            
+
             while (choice != 4)
             {
-               
+                Console.WriteLine("====================");
+                Console.WriteLine($"Векторы равны: {{{vector1.VectorX}:{vector1.VectorY}:{vector1.VectorZ}}} и {{{vector2.VectorX}:{vector2.VectorY}:{vector2.VectorZ}}} ");
+                Console.WriteLine("====================");
+
                 Console.WriteLine("1)Сумма векторов");
                 Console.WriteLine("2)Разность векторов");
                 Console.WriteLine("3)Произведение векторов");
                 Console.WriteLine("4)Выход");
+
+                Console.WriteLine();
                 Console.WriteLine("====================");
                 Console.Write("Введите пункт меню: ");
+
+                Console.ReadKey();
                 choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
                     case 1:
                         Console.WriteLine($"Результат суммы двух трёхмерных векторов: {{{vectorAddition.VectorX}:{vectorAddition.VectorY}:{vectorAddition.VectorZ}}}");
+
                         Console.ReadKey();
                         Console.Clear();
                         break;
@@ -45,7 +53,7 @@ namespace Lab_02_2_
                         Console.WriteLine($"Результат векторного произведения двух трёхмерных векторов: {{{vectorVectorMultiplication.VectorX}:{vectorVectorMultiplication.VectorY}:{vectorVectorMultiplication.VectorZ}}}");
                         Console.ReadKey();
                         Console.Clear();
-                        break;            
+                        break;
                     default:
                         choice = 4;
                         Console.ReadKey();
@@ -54,12 +62,12 @@ namespace Lab_02_2_
                         Console.WriteLine("Выход!");
                         break;
                 }
-                
+
             }
 
 
         }
     }
-    
+
 }
 
