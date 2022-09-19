45d6ac233e1a811295031db444c4370448ba3c00
Calmbl4(laptop) <boss.buyakevich@mail.ru>
2022-09-19 15:34:22 +0300

    added lab 6

---
+++ Labs_02y_01s_OOP/Lab_06/Lab_06/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Subject[] subject = new Subject[Enum.GetNames(typeof(Subject)).Length];
+            Student[] student =
+             {
+                new Student("Громыко Иван",Group.ITI_21,subject),
+                new Student("Гаращук Иван",Group.ITI_21,subject),
+
+                new Student("Свинка Пеппа",Group.ITI_22, subject),
+                new Student("Михалков Вениамин",Group.ITI_22, subject),
+
+                new Student("Месси Глиномесси",Group.ITP_21,subject),
+                new Student("Комраков Кормаков",Group.ITP_21, subject),
+
+                new Student("Жесточайший Кринж",Group.ITP_22,subject),
+                new Student("Суковатый Александр",Group.ITP_22,subject),
+            };
+            bool isWork = true;
+
+            while (isWork)
+            {
+                Console.WriteLine("\tМЕНЮ\t");
+                Console.WriteLine("0)Получить оценки студентов");
+                Console.WriteLine("1)Вывести список студентов и их оценки");
+                Console.WriteLine("2)Вывести по каждому предмету средний балл");
+                Console.WriteLine("3)Вывести по каждой студенческой группе средний балл на ссесии");
+                Console.WriteLine("4)Вывести количество студентов, получивших неудовлетворительные оценки, по указанному предмеиу");
+                Console.WriteLine("5)Выход");
+                int punctOfMenu = Convert.ToInt32(Console.ReadLine());
+                switch (punctOfMenu)
+                {
+                    case 0:
+                        for (int i = 0; i < student.Length; i++)
+                        {
+                            for (int j = 0; j < Enum.GetNames(typeof(Subject)).Length; j++)
+                            {
+                                student[i].Subject[j] = (Subject)random.Next(1, 11);
+
+                            }
+                            Console.WriteLine();
+
+                        }
+
+                        break;
+                    case 1:
+                        for (int i = 0; i < student.Length; i++)
+                        {
+                            Console.WriteLine(student[i].FI + " : " + student[i].Group);
+                            for (int j = 0; j < Enum.GetNames(typeof(Subject)).Length; j++)
+                            {
+                                Console.Write(Enum.GetValues(typeof(Subject)).GetValue(j) + ": ");
+                                Console.WriteLine((int)student[i].Subject[j]);
+
+                            }
+                            Console.WriteLine();
+
+                        }
+                        break;
+                    case 2:
+                        for (int i = 0; i < Enum.GetNames(typeof(Subject)).Length; i++)
+                        {
+                            Student.ShowAverageMarkSubject(student, subject[i], i);
+                        }
+                        break;
+                    case 3:
+                        Student.ShowGroupAverageSsesionMark(student);
+                        break;
+                    case 4:
+                        Student.ShowStudentsWithPoorMark(student, subject);
+                        break;
+                    case 5:
+                        isWork = false;
+                        break;
+                    default:
+
+                        break;
+
+                }
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+
+        }
+    }
+}
---
+++ Labs_02y_01s_OOP/Lab_06/Lab_06/Student.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_06
+{
+    internal class Student
+    {
+        static private int enumGroupCount = Enum.GetNames(typeof(Group)).Length;
+        static private int enumSubjectCount = Enum.GetNames(typeof(Subject)).Length;
+        public string FI { get; set; }
+        public Group Group { get; set; }
+        public Subject[] Subject = new Subject[enumSubjectCount];
+        public Group[] Groups = new Group[enumGroupCount];
+        public Student(string FI, Group Group, Subject[] Subject)
+        {
+            this.FI = FI;
+            this.Group = Group;
+            for (int i = 0; i < enumSubjectCount; i++)
+            {
+                Subject[i] = (Subject)Enum.GetValues(typeof(Subject)).GetValue(i);
+                this.Subject[i] = Subject[i];
+
+            }
+        }
+
+        public Random random = new Random();
+
+        static public void ShowAverageMarkSubject(Student[] student, Subject subject, int index)
+        {
+            double sum = 0;
+            double averageMarkSubject = 0;
+            for (int i = 0; i < student.Length; i++)
+            {
+                sum += (int)student[i].Subject[(int)subject];
+            }
+            averageMarkSubject = sum / student.Length;
+            Console.WriteLine(Enum.GetValues(typeof(Subject)).GetValue(index) + ": " + averageMarkSubject);
+
+        }
+        static public void ShowGroupAverageSsesionMark(Student[] student)
+        {
+            double[] groupSumMark = new double[enumGroupCount];
+            double[] groupAverageMark = new double[enumGroupCount];
+            int[] studentsInOneGroup = new int[enumGroupCount];
+            for (int i = 0; i < enumGroupCount; i++)
+            {
+                for (int j = 0; j < student.Length; j++)
+                {
+                    if (student[j].Group == (Group)Enum.GetValues(typeof(Group)).GetValue(i))
+                    {
+                        for (int k = 0; k < enumSubjectCount; k++)
+                        {
+                            studentsInOneGroup[i]++;
+                            groupSumMark[i] += (double)student[j].Subject[k];
+
+                        }
+                    }
+                }
+            }
+            /*for (int i = 0; i < student.Length; i++)
+            {
+                for (int j = 0; j < enumGroupCount; j++)
+                {
+                    if (student[i].Group == (Group)Enum.GetValues(typeof(Group)).GetValue(j))
+                    {
+                        for (int k = 0; k < enumSubjectCount; k++)
+                        {
+                            studentsInOneGroup[k]++;
+                            groupSumMark[k] += (double)student[i].Subject[k];
+                        }
+                    }
+                }
+            }*/
+            for (int i = 0; i < enumGroupCount; i++)
+            {
+                groupAverageMark[i] = groupSumMark[i] / studentsInOneGroup[i];
+            }
+            for (int i = 0; i < groupAverageMark.Length; i++)
+            {
+                Console.WriteLine(Enum.GetValues(typeof(Group)).GetValue(i) + ": " + groupAverageMark[i]);
+            }
+        }
+        static public void ShowStudentsWithPoorMark(Student[] student, Subject[] subject)
+        {
+            int[] studentsCount = new int[enumSubjectCount];
+
+            for (int i = 0; i < student.Length; i++)
+            {
+                for (int j = 0; j < enumSubjectCount; j++)
+                {
+                    if ((int)student[i].Subject[j] < 4)
+                    {
+                        studentsCount[j]++;
+                    }
+
+                }
+            }
+            Console.WriteLine();
+            for (int i = 0; i < enumSubjectCount; i++)
+            {
+                Console.WriteLine($"Количество студентов получивших неудовлетворительную оценку по предмету {Enum.GetValues(typeof(Subject)).GetValue(i)}: {studentsCount[i]}");
+
+            }
+            Console.WriteLine();
+            for (int i = 0; i < student.Length; i++)
+            {
+
+                for (int j = 0; j < enumSubjectCount; j++)
+                {
+                    Console.Write($"{Enum.GetValues(typeof(Subject)).GetValue(j)}({i + 1})" + ": " + (int)student[i].Subject[j] + "\t");
+
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+
+    enum Subject
+    {
+        Math,
+        History,
+        English,
+        OOP,
+    }
+    enum Group
+    {
+        ITI_21,
+        ITI_22,
+        ITP_21,
+        ITP_22,
+    }
+}
+
