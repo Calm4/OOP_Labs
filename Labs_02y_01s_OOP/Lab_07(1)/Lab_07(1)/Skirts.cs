bd092c01fe2c6b8584eea8c778c1c15381b2b6dc
Calmbl4(laptop) <boss.buyakevich@mail.ru>
2022-09-26 21:36:20 +0300

    7lab

---
+++ Labs_02y_01s_OOP/Lab_07(1)/Lab_07(1)/Skirts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_07_1_
+{
+    internal class Skirts : Costume
+    {
+        public Skirts(Gender gender, double growth, Type_clothes type, Color color) : base(gender, growth, type, color)
+        {
+        }
+    }
+}
